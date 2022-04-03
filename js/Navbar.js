b354ce3d0525ecb4428604b4ceeba44acc30d7e9
2detrebol <maubrandi@gmail.com>
2022-04-03 01:25:17 -0300

    menu hambuerg + efectos varios js

--- js/Navbar.js
+++ js/Navbar.js
@@ -1,11 +1,16 @@
-var navbar = document.querySelector('nav')
+var navbar = document.querySelector("nav");
+var logo = document.querySelector("nav div");
+var burger = document.querySelector("nav div button span");
 
-window.onscroll = function() {
-
+window.onscroll = function () {
   // pageYOffset or scrollY
   if (window.pageYOffset > 0) {
-    navbar.classList.add('scrolled')
+    navbar.classList.add("scrolled");
+    logo.classList.add("scrolled");
+    burger.classList.add("scrolled");
   } else {
-    navbar.classList.remove('scrolled')
+    navbar.classList.remove("scrolled");
+    logo.classList.remove("scrolled");
+    burger.classList.remove("scrolled");
   }
-}
+};
